428609f7f64dd11b57186ed5d09fc29af19bb971
Alexandru Susnea <alexandru.susnea@outlook.com>
2024-10-28 12:14:02 +0200

    Updated exercise files

--- m7/application_config_example/datasources.tf
+++ m7/application_config_example/datasources.tf
@@ -5,3 +5,14 @@
 data "aws_ssm_parameter" "amzn2_linux" {
   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
+
+data "tfe_outputs" "main" {
+  organization = "MY_ORG"
+  workspace = "SOURCE_WORKSPACE"
+}
+
+# All outputs from the selected workspace
+data.tfe_outputs.main.values
+
+# All nonsensitive outputs from the selected workspace
+data.tfe_outputs.main.nonsensitive_values
