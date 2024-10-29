689269058db3abe4ef38f91b5ec30ce7c1c07546
Alexandru Susnea <alexandru.susnea@outlook.com>
2024-10-29 09:47:29 +0200

    Class files update

--- m8/ansible_updates/userdata.sh
+++ m8/ansible_updates/userdata.sh
@@ -3,5 +3,5 @@ sudo yum update
 sudo yum install git -y
 sudo amazon-linux-extras install ansible2 -y
 sudo mkdir /var/ansible_playbooks
-sudo git clone ${playbook_repository} /var/ansible_playbooks
-ansible-playbook /var/ansible_playbooks/playbook.yml -i /var/ansible_playbooks/hosts
+sudo git clone ${playbook_repository} /var/ansible_playbooks # clone the repo
+ansible-playbook /var/ansible_playbooks/playbook.yml -i /var/ansible_playbooks/hosts # run the Playbook
